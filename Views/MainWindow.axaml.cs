aa0fc6843ada4ceaa1b22c966b198ed1357b7ffd
BareGeode <georgiy@live.no>
2024-07-25 17:28:38 +0200

    feat: Add OpenImage function for button to work

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,4 +1,13 @@
+using System;
+using System.IO;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Diagnostics;
+using Avalonia.Interactivity;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform.Storage;
+using DynamicData;
 
 namespace beskriv.Views;
 
@@ -7,5 +16,22 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    public async void OpenImage(object sender, RoutedEventArgs args)
+    {
+        if (Application.Current?.ApplicationLifetime is not IClassicDesktopStyleApplicationLifetime desktop ||
+            desktop.MainWindow?.StorageProvider is not { } provider)
+            throw new NullReferenceException("Missing StorageProvider instance.");
+
+        var files = await provider.OpenFilePickerAsync(new FilePickerOpenOptions()
+        {
+            Title = "Open Image File",
+            AllowMultiple = false
+        });
+
+        var newPath = files[0].TryGetLocalPath();
+        if (newPath != null)
+            ImageDisplay.Source = new Bitmap(newPath);
     }
 }
